aafc7db87a38a0636669a9f5165b9ecc737974af
minimo <minimo.mobile@gmail.com>
2017-06-16 15:00:08 +0900

    Update.

--- src/scene/gamescene.js
+++ src/scene/gamescene.js
@@ -16,7 +16,7 @@ phina.define("stg.GameScene", {
             width: SC_W,
             height: SC_H,
         }).addChildTo(this);
-        this.glLayer.setScale(2);
+        this.glLayer.setOrigin(0, 0);
 
         this.glCamera = this.glLayer.camera;
         this.glRenderer = this.glLayer.renderer;
@@ -26,9 +26,10 @@ phina.define("stg.GameScene", {
         this.glRenderer.setClearColor(0x8080B0);
 
         //カメラ位置の変更
-        this.glCamera.position.x = -20;
+        this.glCamera.position.x = 0;
         this.glCamera.position.y = 100;
-        this.glCamera.position.z = 160;
+        this.glCamera.position.z = 100;
+        this.glCamera.lookAt(new THREE.Vector3(0, 0, 0));
 
         //ライトの追加
         this.glScene.remove(this.glLayer.light);
@@ -39,33 +40,39 @@ phina.define("stg.GameScene", {
 
         //物理演算設定
         this.phyWorld = new CANNON.World();
-        this.phyWorld.gravity.set(0, -10, 0);
+        this.phyWorld.gravity.set(0, -9.82, 0);
         this.phyWorld.broadphase = new CANNON.NaiveBroadphase();
-        this.phyWorld.solver.iterations = 8;
+        this.phyWorld.solver.iterations = 10;
         this.phyWorld.solver.tolerance = 0.1;
 
         //床作成
-        let planeGeometory = new THREE.PlaneGeometry(100, 100, 1);
+        let planeGeometory = new THREE.PlaneGeometry(200, 200, 10, 10);
         let planeMaterial = new THREE.MeshLambertMaterial({color: 0xaaaaaa});
         this.plane = new THREE.Mesh(planeGeometory, planeMaterial);
+        this.plane.useQuaternion = true;
         this.glScene.add(this.plane);
 
         let planeMass = 0;
         let planeShape = new CANNON.Plane();
-        this.plane.physics = new CANNON.Body({mass: planeMass, shape: planeShape});
+        this.plane.physics = new CANNON.Body({
+            mass: planeMass,
+            position: new CANNON.Vec3(0, -20, 0),
+            shape:planeShape
+        });
         this.plane.physics.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
         this.phyWorld.add(this.plane.physics);
 
         //箱作成
-        let geometory = new THREE.CubeGeometry(10, 10, 10);
+        let geometory = new THREE.CubeGeometry(5, 5, 5);
         let material = new THREE.MeshLambertMaterial({color: 0x44aa22});
         this.cube = new THREE.Mesh(geometory, material);
+        this.cube.useQuaternion = true;
         this.glScene.add(this.cube);
 
         let boxMass = 1;
         let boxShape = new CANNON.Box(new CANNON.Vec3(5, 5, 5));
         this.cube.physics = new CANNON.Body({mass: boxMass, shape: boxShape});
-        this.cube.physics.angularVelocity.set(0, 5, 10);
+        this.cube.physics.angularVelocity.set(0, 0, 0);
         this.cube.physics.angularDamping = 0.1;
         this.phyWorld.add(this.cube.physics);
     },
@@ -74,10 +81,10 @@ phina.define("stg.GameScene", {
         this.phyWorld.step(1 / 60);
 
         //箱の姿勢位置情報更新
-        this.cube.position.copy(this.cube.physics.position);  
-        this.cube.quaternion.copy(this.cube.physics.quaternion);  
+        this.cube.position.copy(this.cube.physics.position);
+        this.cube.quaternion.copy(this.cube.physics.quaternion); 
 
-        this.plane.position.copy(this.plane.physics.position);  
-        this.plane.quaternion.copy(this.cube.physics.quaternion);  
+        this.plane.position.copy(this.plane.physics.position);
+        this.plane.quaternion.copy(this.cube.physics.quaternion);
     },
 });
