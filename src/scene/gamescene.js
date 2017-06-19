61373bf5960f33ab21ab099a19ca071465bdaa87
minimo <minimo.mobile@gmail.com>
2017-06-19 13:28:57 +0900

    Update.

--- src/scene/gamescene.js
+++ src/scene/gamescene.js
@@ -22,15 +22,23 @@ phina.define("stg.GameScene", {
         this.glRenderer = this.glLayer.renderer;
         this.glScene = this.glLayer.scene;
 
+        this.glRenderer.shadowMap.enabled = true
+
+        //Orbit control
+        this.control =  new THREE.OrbitControls(this.glCamera);
+
         //背景色設定
         this.glRenderer.setClearColor(0x8080B0);
 
         this.setup3DLayer();
         this.setup3DWorld();
         this.setup3DObject();
+
+        this.time = 0;
     },
 
     update: function(app) {
+        this.control.update();
         this.phyWorld.step(1 / 60);
 
         //箱の姿勢位置情報更新
@@ -38,21 +46,24 @@ phina.define("stg.GameScene", {
             this.cube.position.copy(this.cube.physics.position);
             this.cube.quaternion.copy(this.cube.physics.quaternion);
         }
+
+        this.time++;
     },
 
     setup3DLayer: function() {
         //カメラ位置の変更
         this.glCamera.position.x = 0;
-        this.glCamera.position.y = 30;
-        this.glCamera.position.z = 30;
+        this.glCamera.position.y = 50;
+        this.glCamera.position.z = 100;
         this.glCamera.lookAt(new THREE.Vector3(0, 0, 0));
 
         //ライトの追加
         this.glScene.remove(this.glLayer.light);
         this.glScene.add(new THREE.AmbientLight(0xFFFFFF));
         this.light = new THREE.DirectionalLight(0xaaaaaa, 1);
-        this.light.position.set(-1, 1, -1);
-        this.light.rotation.set(0, Math.PI / 2, 0);
+        this.light.position.set(-100, 100, 100);
+//        this.light.rotation.set(0, 1, 0);
+        this.light.castShadow = true;
         this.glScene.add(this.light);
     },
 
@@ -73,27 +84,28 @@ phina.define("stg.GameScene", {
         phyPlane.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
         this.phyWorld.add(phyPlane);
 
-        let planeGeometory = new THREE.PlaneGeometry(200, 200, 10, 10);
+        let planeGeometory = new THREE.PlaneGeometry(100, 100, 100, 100);
         let planeMaterial = new THREE.MeshLambertMaterial({color: 0xaaaaaa});
         this.plane = new THREE.Mesh(planeGeometory, planeMaterial);
         this.plane.position.set(0, 0, 0);
-        this.plane.rotation.set( -Math.PI / 2, 0, 0);
+        this.plane.rotation.set(-Math.PI / 2, 0, 0);
         this.plane.physics = phyPlane;
+        this.plane.receiveShadow = true;
         this.glScene.add(this.plane);
 
         //箱の作成
         let phyBox = new CANNON.Body({mass: 1});
-        phyBox.addShape(new CANNON.Box(new CANNON.Vec3(5, 5, 5)));
-        phyBox.position.y = 10;
-        phyBox.angularVelocity.set(10, 0, 0);
-        phyBox.angularDamping = 0.1;
+        phyBox.addShape(new CANNON.Box(new CANNON.Vec3(10, 10, 10)));
+        phyBox.position.y = 50;
+        phyBox.angularVelocity.set(10, 10, 10);
+        phyBox.angularDamping = 0.5;
         this.phyWorld.add(phyBox);
 
-        let geometory = new THREE.CubeGeometry(5, 5, 5);
+        let geometory = new THREE.CubeGeometry(10, 10, 10);
         let material = new THREE.MeshLambertMaterial({color: 0x44aa22});
         this.cube = new THREE.Mesh(geometory, material);
         this.cube.physics = phyBox;
-        this.cube.position.set(0, 5, 0);
+        this.cube.castShadow = true;
         this.glScene.add(this.cube);
     },
 });
