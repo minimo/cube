408ef62a57da58801999e2a1c2d640e2ddfe609b
minimo <minimo.mobile@gmail.com>
2017-07-11 18:05:00 +0900

    Update scene.

--- src/scene/gamescene.js
+++ src/scene/gamescene.js
@@ -38,6 +38,10 @@ phina.define("stg.GameScene", {
     },
 
     update: function(app) {
+        if (this.time % 10 == 0) {
+            this.addCube();
+        }
+
         this.control.update();
         this.phyWorld.step(1 / 60);
         this.time++;
@@ -47,7 +51,7 @@ phina.define("stg.GameScene", {
         //カメラ位置の変更
         this.glCamera.position.x = 0;
         this.glCamera.position.y = 50;
-        this.glCamera.position.z = 100;
+        this.glCamera.position.z = 200;
         this.glCamera.lookAt(new THREE.Vector3(0, 0, 0));
 
         //ライトの追加
@@ -63,7 +67,7 @@ phina.define("stg.GameScene", {
     setup3DWorld: function() {
         //物理演算設定
         this.phyWorld = new CANNON.World();
-        this.phyWorld.gravity.set(0, -9.82, 0);
+        this.phyWorld.gravity.set(0, -9.82*10, 0);
         this.phyWorld.broadphase = new CANNON.NaiveBroadphase();
         this.phyWorld.solver.iterations = 10;
         this.phyWorld.solver.tolerance = 0.1;
@@ -75,10 +79,10 @@ phina.define("stg.GameScene", {
         phyPlane.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
         this.phyWorld.add(phyPlane);
 
-        let planeGeometory = new THREE.PlaneGeometry(100, 100, 100, 100);
+        let planeGeometory = new THREE.PlaneGeometry(1000, 1000, 100, 100);
         let planeMaterial = new THREE.MeshLambertMaterial({color: 0xaaaaaa});
         this.plane = new THREE.Mesh(planeGeometory, planeMaterial);
-        this.plane.position.set(0, 5, 0);
+        this.plane.position.set(0, 0, 0);
         this.plane.rotation.set(-Math.PI / 2, 0, 0);
         this.plane.physics = phyPlane;
         this.plane.receiveShadow = true;
@@ -86,11 +90,19 @@ phina.define("stg.GameScene", {
     },
 
     setup3DObject: function() {
+    },
+
+    addCube: function() {
         let geometory = new THREE.CubeGeometry(10, 10, 10);
         let material = new THREE.MeshLambertMaterial({color: 0x44aa22});
+
         var cb = phina.extension.ThreeElement(new THREE.Mesh(geometory, material));
-        var ph = phina.extension.Physics(this.phyWorld).attachTo(cb);
-        cb.setPosition(0, 50, 0);
+        var ph = phina.extension.Physics(this.phyWorld,{
+            type: "box",
+            size: {x: 5, y: 5, z: 5},
+            mass: 10,
+        }).attachTo(cb);
+        cb.setPosition(0, 100, 0);
         cb.addChildTo(this.glLayer);
         this.glScene.add(cb.threeObj);
     },
